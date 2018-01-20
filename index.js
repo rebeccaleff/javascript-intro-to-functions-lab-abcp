de4ee065752807506d48bc1794533397ca63012a
Rebecca Leff <rebeccacleff@gmail.com>
2018-01-20 00:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma (string) {
   if (string === string.toLowerCase()) return ("I can't hear you!")
     else if (string === string.toUpperCase()) return ("YES INDEED!")
     else if (string === "I love you, Grandma.") return ("I love you, too.")
-
+}
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
